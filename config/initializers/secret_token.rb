3ce0b6cbbd5c6649a0ae8fd67b7f55b06abf07ff
Alice - dev <alice-dev@Alices-MacBook-Pro.local>
2013-07-29 07:10:58 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luggish::Application.config.secret_token = '8f41d4ffeffe3adb34d74a837766210cbe2bed54cd6b64ab022ec83f8fbab04b96ec88fcd3f8bbbb366bb15efa3953c483f45d6b3a01e262e55430afd3b67dd6'
