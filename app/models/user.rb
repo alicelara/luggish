010167fe4907a5f128a53fa101e5e745720bd9e2
Zehramirza <zehrabeg@gmail.com>
2013-07-29 23:34:33 -0400

    created password reset and added remember_me migration file as it was forgotten

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
 
   attr_accessible :email, :password, :password_confirmation
+  
+  
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Luggish::Application.routes.draw do
   
+  get "password_resets/create"
+
+  get "password_resets/edit"
+
+  get "password_resets/update"
+  get "password_resets/show"
+
+
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
 
  resources :users
  resources :sessions
-
+ resources :password_resets
  root :to => 'pages#home'
  get 'about' => 'pages#about'
 
