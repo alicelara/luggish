94724f750fd10678044c12f6185475b70c573b63
Alice Lara <hello@alicelara.ca>
2013-07-31 16:34:08 -0400

    migrating the models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130731015945) do
+ActiveRecord::Schema.define(:version => 20130731202426) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "item_lists", :force => true do |t|
     t.integer  "item_id"
