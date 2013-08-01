87c173c60975bdf1f64b057ad49d94b7de37991d
Alice Lara <hello@alicelara.ca>
2013-07-31 20:02:47 -0400

    create tags model and associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :lists, through: :item_lists
+  has_and_belongs_to_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130731202426) do
+ActiveRecord::Schema.define(:version => 20130731235209) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(:version => 20130731202426) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "items_tags", :id => false, :force => true do |t|
+    t.integer  "item_id",    :null => false
+    t.integer  "tag_id",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "lists", :force => true do |t|
     t.string   "name"
     t.string   "location"
@@ -43,6 +50,12 @@ ActiveRecord::Schema.define(:version => 20130731202426) do
     t.date     "end_date"
     t.string   "sex"
     t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
